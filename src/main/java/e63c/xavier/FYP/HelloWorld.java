83c8af23370093a7e09ad031b0579fe1c6852c4d
fredr <fredr@10.174.121.184>
2023-04-26 14:47:08 +0800

    Fredrick Test

--- src/main/java/e63c/xavier/FYP/HelloWorld.java
+++ src/main/java/e63c/xavier/FYP/HelloWorld.java
@@ -26,6 +26,8 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World!123");
 		System.out.println("Hello World!abc1");
+		System.out.println("Hello World!testing");
+		
 
 	}
 
