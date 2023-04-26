b16bdbd7c6f66933785136da55d8f9edb9537997
21041531 <21041531@10.175.4.101>
2023-04-26 14:37:46 +0800

    buddy test

--- src/main/java/e63c/xavier/FYP/HelloWorld.java
+++ src/main/java/e63c/xavier/FYP/HelloWorld.java
@@ -25,7 +25,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello World!123");
-		System.out.println("Hello World!abc");
+		System.out.println("Hello World!abc1");
 
 	}
 
